774d600175421359fd39b3311d83e994ee1d644c
EleventhTen083-ASC <81180062+EleventhTen083-ASC@users.noreply.github.com>
2021-04-13 22:28:01 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyBoiQiQIIoSbGZFDKhjXFYCYf9NZbc2DwQ",
+    authDomain: "autentifica42scja.firebaseapp.com",
+    projectId: "autentifica42scja",
+    storageBucket: "autentifica42scja.appspot.com",
+    messagingSenderId: "785969402361",
+    appId: "1:785969402361:web:2993d68146e5de0b206d28",
+    measurementId: "G-KMM8QVLV4D"
+  };
