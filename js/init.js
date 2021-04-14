27b0c1a56b90de0e2cee08972c944eaacb823671
EleventhTen083-ASC <81180062+EleventhTen083-ASC@users.noreply.github.com>
2021-04-14 00:20:22 -0500

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyBoiQiQIIoSbGZFDKhjXFYCYf9NZbc2DwQ",
-    authDomain: "autentifica42scja.firebaseapp.com",
-    projectId: "autentifica42scja",
-    storageBucket: "autentifica42scja.appspot.com",
-    messagingSenderId: "785969402361",
-    appId: "1:785969402361:web:2993d68146e5de0b206d28",
-    measurementId: "G-KMM8QVLV4D"
-  };
+    apiKey: "AIzaSyDdapj3H3Dd2w9DHpLHwbzwpwSwU7rItpg",
+    authDomain: "chatrickfb.firebaseapp.com",
+    projectId: "chatrickfb",
+    storageBucket: "chatrickfb.appspot.com",
+    messagingSenderId: "815561540833",
+    appId: "1:815561540833:web:2cf0f52c625e8e281723a0",
+    measurementId: "G-5W02XQR53J"
+    });
